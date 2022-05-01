b8c544cfd704fcda61510e3be72bfc58871b2e4e
Matthew Nichols <matthew@matthew-nichols.com>
2022-05-01 23:35:47 +0200

    Added layout, css & clientside script.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy({"./src/styles/": "styles"});
+  eleventyConfig.addPassthroughCopy({"./src/client-side-js/": "script"});
+
   return {
     dir: {
       input: "src"
